5b42e72fed3b7b121413cd95db010cfcdd57bb47
Shivani-1524 <67316487+Shivani-1524@users.noreply.github.com>
2022-06-07 23:54:01 +0530

    feat: edit post

--- src/Features/postsSlice.js
+++ src/Features/postsSlice.js
@@ -4,46 +4,81 @@ import { getTrendingPosts } from "../Utils/getTrendingPosts";
 
 const namespace = 'posts'
 
-export const getPosts = createAsyncThunk(`/${namespace}/getPosts`, async () => {
-    const { data } = await axios.get("/api/posts")
-    return data.posts
+
+export const getPosts = createAsyncThunk(`/${namespace}/getPosts`, async (_, { rejectWithValue }) => {
+    try {
+        const { data } = await axios.get("/api/posts")
+        return data.posts
+    } catch (err) {
+        return rejectWithValue(err.response.data);
+    }
 })
 export const getUserPosts = createAsyncThunk(`/${namespace}/getPosts`, async (username) => {
     const { data } = await axios.get(`/api/posts/user/${username}`)
     return data.posts
 })
-export const createPost = createAsyncThunk(`/${namespace}/getPosts`, async () => {
-    const { data } = await axios.get("/api/posts")
-    return data.posts
+export const createPost = createAsyncThunk(`/${namespace}/getPosts`, async ({ postData }, { rejectWithValue }) => {
+    console.log(postData);
+    try {
+        const { data } = await axios.post("/api/posts", { postData });
+        console.log(data.posts);
+        return data.posts;
+    } catch (err) {
+        return rejectWithValue(err.response.data);
+    }
 })
-export const deletePost = createAsyncThunk(`/${namespace}/getPosts`, async (postId) => {
-    const { data } = await axios.get(`/api/posts/${postId}`)
-    return data.posts
+export const deletePost = createAsyncThunk(`/${namespace}/deletePost`, async (postId, { rejectWithValue }) => {
+    try {
+        const { data } = await axios.delete(`/api/posts/${postId}`)
+        return data.posts
+    } catch (err) {
+        return rejectWithValue(err.response.data);
+    }
 })
-export const editPost = createAsyncThunk(`/${namespace}/getPosts`, async (postId) => {
-    const { data } = await axios.get(`/api/posts/edit/${postId}`)
-    return data.posts
+export const editPost = createAsyncThunk(`/${namespace}/editPost`, async ({ postId, postData }, { rejectWithValue }) => {
+    try {
+        const { data } = await axios.post(`/api/posts/edit/${postId}`, { postData })
+        return data.posts
+    } catch (err) {
+        return rejectWithValue(err.response.data);
+    }
 })
+export const likePost = createAsyncThunk(`/${namespace}/likePost`, async (postId, { rejectWithValue }) => {
+    try {
+        const { data } = await axios.post(`/api/posts/like/${postId}`)
+        console.log("like data", data)
+        return data.posts
+    } catch (err) {
+        return rejectWithValue(err.response.data)
+    }
 
-
-
-export const likePost = createAsyncThunk(`/${namespace}/likePost`, async (postId) => {
-    const { data } = await axios.post(`/api/posts/like/${postId}`)
-    return data.posts
 })
-export const dislikePost = createAsyncThunk(`/${namespace}/dislikePost`, async (postId) => {
-    const { data } = await axios.post(`/api/posts/dislike/${postId}`)
-    return data.posts
+export const dislikePost = createAsyncThunk(`/${namespace}/dislikePost`, async (postId, { rejectWithValue }) => {
+    try {
+        const { data } = await axios.post(`/api/posts/dislike/${postId}`)
+        return data.posts
+    }
+    catch (err) {
+        return rejectWithValue(err.response.data);
+    }
 })
 
 //boomarks
-export const bookmarkPost = createAsyncThunk(`/${namespace}/bookmarkPost`, async (postId) => {
-    const { data } = await axios.post(`/api/users/bookmark/${postId}`)
-    return data
+export const bookmarkPost = createAsyncThunk(`/${namespace}/bookmarkPost`, async (postId, { rejectWithValue }) => {
+    try {
+        const { data } = await axios.post(`/api/users/bookmark/${postId}`)
+        return data
+    } catch (err) {
+        return rejectWithValue(err.response.data);
+    }
 })
-export const removeBookmark = createAsyncThunk(`/${namespace}/removeBookmark`, async (postId) => {
-    const { data } = await axios.post(`/api/users/remove-bookmark/${postId}`);
-    return data
+export const removeBookmark = createAsyncThunk(`/${namespace}/removeBookmark`, async (postId, { rejectWithValue }) => {
+    try {
+        const { data } = await axios.post(`/api/users/remove-bookmark/${postId}`);
+        return data
+    } catch (err) {
+        return rejectWithValue(err.response.data);
+    }
 })
 export const getAllBookmarks = createAsyncThunk(`/${namespace}/getBookmarks`, async () => {
     const { data } = await axios.get(`/api/users/bookmark`)
@@ -70,15 +105,17 @@ export const deleteComment = createAsyncThunk(`/${namespace}/dislikePost`, async
 })
 
 
-
 const initialStateValue = {
     posts: [],
     bookmarks: [],
+    gifUrlSelected: '',
+    sortedBy: 'newest',
     postStatus: null,
     loadCommentStatus: null,
     likeStatus: { status: '', loadId: null },
     bookmarkStatus: { status: '', loadId: null },
 }
+
 export const postsSlice = createSlice({
     name: namespace,
     initialState: initialStateValue,
@@ -87,28 +124,70 @@ export const postsSlice = createSlice({
             switch (action.payload) {
                 case "trending":
                     state.posts = getTrendingPosts(current(state.posts))
+                    state.sortedBy = "trending"
                     break;
                 case 'newest':
                     state.posts.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
+                    state.sortedBy = "newest"
                     break;
                 case 'oldest':
                     state.posts.sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt))
+                    state.sortedBy = "oldest"
                     break;
                 default:
-                    return state.posts;
+                    return state.posts.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
             }
         },
+        gifSelection: (state, action) => {
+            state.gifUrlSelected = action.payload
+        }
     },
     extraReducers: {
         [getPosts.pending]: (state) => {
-            state.status = 'loading'
+            state.postStatus = 'loading'
         },
         [getPosts.fulfilled]: (state, { payload }) => {
-            state.posts = payload.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
-            state.status = 'success'
+            state.posts = payload
+            state.postStatus = 'success'
         },
         [getPosts.rejected]: (state) => {
-            state.status = 'failed'
+            state.postStatus = 'failed'
+        },
+
+        [createPost.pending]: (state) => {
+            state.postStatus = 'loading'
+        },
+        [createPost.fulfilled]: (state, { payload }) => {
+            state.posts = payload
+            state.postStatus = 'success'
+        },
+        [createPost.rejected]: (state, { payload }) => {
+            state.postStatus = 'failed'
+            console.log(payload)
+        },
+
+        [deletePost.pending]: (state) => {
+            state.postStatus = 'loading'
+        },
+        [deletePost.fulfilled]: (state, { payload }) => {
+            state.posts = payload
+            state.postStatus = 'success'
+        },
+        [deletePost.rejected]: (state, { payload }) => {
+            state.postStatus = 'failed'
+            console.log(payload)
+        },
+
+        [editPost.pending]: (state) => {
+            state.postStatus = 'loading'
+        },
+        [editPost.fulfilled]: (state, { payload }) => {
+            state.posts = payload
+            state.postStatus = 'success'
+        },
+        [editPost.rejected]: (state, { payload }) => {
+            state.postStatus = 'failed'
+            console.log(payload)
         },
 
 
@@ -117,7 +196,8 @@ export const postsSlice = createSlice({
             likeStatus.loadId = meta.arg;
         },
         [likePost.fulfilled]: (state, { payload }) => {
-            state.posts = payload;
+            console.log("LIKE FULFILLED")
+            state.posts = payload
             state.likeStatus.status = 'success';
         },
         [likePost.rejected]: ({ likeStatus }) => {
@@ -130,7 +210,7 @@ export const postsSlice = createSlice({
             likeStatus.loadId = meta.arg;
         },
         [dislikePost.fulfilled]: (state, { payload }) => {
-            state.posts = payload;
+            state.posts = payload
             state.likeStatus.status = 'success';
         },
         [dislikePost.rejected]: ({ likeStatus }) => {
@@ -144,12 +224,11 @@ export const postsSlice = createSlice({
         },
         [removeBookmark.fulfilled]: (state, { payload }) => {
             // state.posts = payload;
-            state.bookmarks = payload.bookmarks;
+            state.bookmarks = payload.bookmarks.reverse();
             state.bookmarkStatus.status = 'success';
         },
         [removeBookmark.rejected]: (state, { payload }) => {
             state.bookmarkStatus.status = 'failed'
-            console.log(payload)
         },
 
 
@@ -158,13 +237,11 @@ export const postsSlice = createSlice({
             bookmarkStatus.loadId = meta.arg;
         },
         [bookmarkPost.fulfilled]: (state, { payload }) => {
-            console.log(payload)
-            state.bookmarks = payload.bookmarks;
+            state.bookmarks = payload.bookmarks.reverse();
             state.bookmarkStatus.status = 'success';
         },
         [bookmarkPost.rejected]: ({ bookmarkStatus }, { payload }) => {
             bookmarkStatus.status = 'failed'
-            console.log(payload)
         },
 
 
@@ -172,7 +249,7 @@ export const postsSlice = createSlice({
             state.status = 'loading'
         },
         [getAllBookmarks.fulfilled]: (state, { payload }) => {
-            state.posts = payload.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
+            state.posts = payload.reverse()
             state.status = 'success'
         },
         [getAllBookmarks.rejected]: (state, { payload }) => {
@@ -181,5 +258,5 @@ export const postsSlice = createSlice({
     }
 })
 
-export const { sortPosts } = postsSlice.actions
+export const { sortPosts, gifSelection } = postsSlice.actions
 export default postsSlice.reducer

8db34ba1224cf0a7d73805989045ecf3f5e6b2d5
Shivani-1524 <67316487+Shivani-1524@users.noreply.github.com>
2022-06-07 23:51:55 +0530

    fix; sorting bug

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,13 +1,22 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import './homepage.css'
 import { UserPost } from '../../Components/UserPost/UserPost'
 import { useSelector, useDispatch } from 'react-redux'
 import { getPosts } from '../../Features/postsSlice'
 import { CreatePost } from '../../Components/CreatePost/CreatePost'
-
+import { sortPosts } from '../../Features/postsSlice'
 const HomePage = () => {
-    const { posts, status: postFetchStatus } = useSelector((state) => state.posts)
+    const { posts, postStatus, sortedBy } = useSelector((state) => state.posts)
     const dispatch = useDispatch()
+
+    if (sortedBy === 'oldest') {
+        dispatch(sortPosts('oldest'))
+    } else if (sortedBy === 'trending') {
+        dispatch(sortPosts('trending'))
+    } else {
+        dispatch(sortPosts('newest'))
+    }
+
     useEffect(() => {
         dispatch(getPosts())
     }, [])
@@ -16,9 +25,9 @@ const HomePage = () => {
         <div className='homepage-container'>
             <CreatePost />
             <div className="posts-listing flex-col mg-t-50">
-                {postFetchStatus === 'success' && posts.map((post) =>
+                {postStatus === 'success' ? posts.map((post) =>
                     <UserPost props={post} key={post._id} />
-                )}
+                ) : <p>Loading Posts...</p>}
             </div>
         </div>
     )
